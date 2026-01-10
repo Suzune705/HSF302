ee29092cffd6368b1cbcfaeb6936d58a1c24cb07
Suzune <124695777+Suzune705@users.noreply.github.com>
2026-01-10 17:18:51 +0700

    feat : mapping data

---
+++ demo2/src/main/java/com/demo2/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.demo2.mapper;
+
+
+import com.demo2.model.dto.response.UserResponse;
+import com.demo2.model.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserMapper {
+
+    public static UserResponse toUserResponse(User user){
+        return UserResponse.builder()
+                .userID(user.getUserID())
+                .name(user.getName())
+                .account(user.getAccount())
+                .build();
+    }
+}
